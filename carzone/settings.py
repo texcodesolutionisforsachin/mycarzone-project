61dbd1f895c1876de651af771309c4cf1d5128ee
myseeproject <kamblesachin65@gmail.com>
2025-10-07 15:09:06 +0530

    Completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,8 +116,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
+
+STATICFILES_DIRS=[
+                  os.path.join(BASE_DIR,'carzone/static')
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
